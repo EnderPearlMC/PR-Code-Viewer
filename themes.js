4e88a0b28aaa62a07740aabe8b00ac03e68148d3
animatek <51943119+animatek38@users.noreply.github.com>
2022-03-21 19:39:02 +0100

    l

---
+++ themes.js
@@ -0,0 +1,32 @@
+var btn = document.getElementById('dark_theme_btn');
+var canvas = document.querySelector('canvas');
+var html = document.querySelector('html')
+var resetBtn = document.getElementById("restart")
+var inputBtn = document.querySelector('input[type=file]');
+console.log(inputBtn);
+let isBlack = false;
+
+
+btn.addEventListener("click", () =>{
+    canvas.style.removeProperty("background-color");
+    html.style.removeProperty("background-color");
+    resetBtn.style.removeProperty("color");
+    inputBtn.style.removeProperty('color');
+    if(isBlack)
+    {
+        canvas.style.backgroundColor = "##e0e0e0";
+        html.style.backgroundColor = "##e0e0e0";
+        resetBtn.style.color = "#1c1b22";
+        inputBtn.style.color = "#1c1b22";
+        isBlack = false;
+        console.log('white');
+    } else if(!isBlack)
+    {
+    console.log('black');
+    canvas.style.backgroundColor = "#1c1b22";
+    html.style.backgroundColor = "#1c1b22";
+    resetBtn.style.color = "#e0e0e0";
+    inputBtn.style.color = "#e0e0e0";   
+    isBlack = true;
+    }
+})
---
+++ translate.js
@@ -0,0 +1,83 @@
+/* 
+
+    Code par NATHAN MARCHIORI 
+
+*/
+
+let input = document.querySelector("input");
+
+
+//le boutton pour choisir le fichier
+input.addEventListener("change", () => {
+  let files = input.files;
+  if (files.length == 0) return;
+  const file = files[0];
+
+  if (!file.name.endsWith(".pr")) {
+    alert("please use a .pr file");
+    return;
+  }
+  console.log("File chosen");
+  let reader = new FileReader();
+  reader.onload = (e) => {
+    const file = e.target.result;
+    // contenue du fichier ligne par ligne en array
+    const lines = file.split(/\r\n|\n/);
+    setActions(translate(lines));
+
+  };
+
+  reader.onerror = (e) => alert(e.target.error.name);
+  reader.readAsText(file);
+});
+
+//pour trouver c quelle function + resultat finale
+
+function translate(text) {
+    let result = [];
+  
+    for (let i = 0; i < text.length; i++) {
+  
+      const element = text[i];
+      if (element.startsWith("MOVE")) {
+          result.push(moveT(element));
+      } else if (element.startsWith("TOOL")) {
+          result.push(tool(element))
+      }
+    }
+  
+    return result;
+}
+
+//pour traduire les function MOVE
+
+function moveT(moveElement) {
+  moveElement = moveElement.split(" ");
+  let result = {
+    name: moveElement[0].toLowerCase(),
+    x: parseInt(moveElement[1].split(":")[1]),
+    y: parseInt(moveElement[2].split(":")[1]),
+    s: parseInt(moveElement[3].split(":")[1]),
+    t: moveElement[4].split(":")[1],
+  };
+
+  if (result.t == "cubic") {
+    result.ax = parseInt(moveElement[5].split(":")[1]);
+    result.ay = parseInt(moveElement[6].split(":")[1]);
+    result.bx = parseInt(moveElement[7].split(":")[1]);
+    result.by = parseInt(moveElement[8].split(":")[1]);
+  }
+
+  return result;
+}
+
+//pour traduire les functions TOOL
+function tool(toolElement) {
+    toolElement = toolElement.split(" ");
+    let result = {
+        name: toolElement[0].toLowerCase(),
+        t: toolElement[1].split(":")[1].toLowerCase(),
+        p: toolElement[2].split(":")[1].toLowerCase(),
+    }
+    return result
+}
