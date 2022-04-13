7a434b787d2da12ba9dd8adbbd3576afb86292dc
animatek <nathtaktak@gmail.com>
2022-04-13 13:15:21 +0200

    server.js now work

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-const express = require "express"
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('static'))
+
+app.get('/', (req, res) => {
+    res.send()
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
