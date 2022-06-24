ea90a16c67de3f2c5957235e50b61a7c7169b070
animatek <nathtaktak@gmail.com>
2022-06-24 16:31:54 +0200

    canny edge detection

--- cannyedgedetection.py
+++ cannyedgedetection.py
@@ -3,15 +3,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread("test.png", cv2.IMREAD_GRAYSCALE)
-lap = cv2. Laplacian(img, cv2.CV_64f, ksize=3)
+lap = cv2.Laplacian(img, cv2.CV_16S, ksize=3)
 lap = np.uint
 canny = cv2.Canny(img, 100, 200)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 titles = ['img', 'canny']
-images = [img, canny]
-for i in range(2):
-    plt.subplot(1, 2, i+1), plt.imshow(images[i], 'gray')
+images = [hsv]
+for i in range(1):
+    plt.subplot(1, 2, i+1), plt.imshow(images[i])
     plt.title(titles[i])
     plt.xticks([]), plt.yticks([])
+
+
 
 plt.show()
---
+++ oscour.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import numpy as np
+cap = cv.VideoCapture(0)
+while(1):
+    frame = cv.imread("test.png")
+    # Take each frame
+    # Convert BGR to HSV
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    # define range of blue color in HSV
+    lower_blue = np.array([0,0,0])
+    upper_blue = np.array([0,0,255])
+    # Threshold the HSV image to get only blue colors
+    mask = cv.inRange(hsv, lower_blue, upper_blue)
+    # Bitwise-AND mask and original image
+    res = cv.bitwise_and(frame,frame, mask= mask)
+    cv.imshow('frame',frame)
+    cv.imshow('mask',mask)
+    cv.imshow('res',res)
+    k = cv.waitKey(5) & 0xFF
+    if k == 27:
+        break
