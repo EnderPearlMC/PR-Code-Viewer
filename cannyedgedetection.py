c40eba33d6dfb95bd8ed9c1d044dd36f179352dd
animatek <51943119+animatek38@users.noreply.github.com>
2022-07-09 18:31:34 +0200

    edge detection for color

--- cannyedgedetection.py
+++ cannyedgedetection.py
@@ -7,10 +7,10 @@ img = cv2.imread("test.png", cv2.IMREAD_GRAYSCALE)
 lap = cv2.Laplacian(img, cv2.CV_16S, ksize=3)
 lap = np.uint
 canny = cv2.Canny(img, 100, 200)
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-titles = ['img', 'canny']
-images = [hsv]
+titles = ['img', 'canny', 'gray']
+images = [img, canny, lap]
 for i in range(1):
     plt.subplot(1, 2, i+1), plt.imshow(images[i])
     plt.title(titles[i])
